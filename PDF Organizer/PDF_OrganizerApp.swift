2adc514fd615699f5c4edb3c149c214a91cf1f8f
Carsten <hello@carsten.dev>
2023-06-21 18:51:48 +0200

    Initial commit

---
+++ PDF Organizer/PDF_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_OrganizerApp.swift
+//  PDF Organizer
+//
+//  Created by Carsten Voß on 21.06.23.
+//
+
+import SwiftUI
+
+@main
+struct PDF_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
